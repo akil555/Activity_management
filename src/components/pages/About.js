ef3a66d0ffa14376fa3f034afe880cc7335c802e
akil555 <107399077+akil555@users.noreply.github.com>
2023-07-15 19:45:04 +0530

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          Welcome to the KEI IIC Activity Management System. This platform allows you to efficiently manage and organize various activities conducted by KEI IIC. With our user-friendly interface and powerful features, you can streamline your workflow and ensure successful execution of events.
+        </p>
+
+        <p className="lead">
+          Our platform provides comprehensive tools for activity planning, participant registration, resource allocation, and result analysis. Whether it's workshops, seminars, or competitions, you can easily create, track, and evaluate all types of activities. Say goodbye to manual paperwork and embrace digital management for enhanced productivity.
+        </p>
+        <p className="lead">Join us today and experience a new level of efficiency in KEI IIC activity management. Let's work together to create impactful and memorable events for our community!</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
